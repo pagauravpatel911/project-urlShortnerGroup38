b61095f99f41ce7092b3904d67b21a99f55184e8
gaurav <pgauravpatel911@gmail.com>
2022-04-04 18:53:01 +0530

    fisr-day

---
+++ src/models/urlModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+
+
+const urlSchema = new mongoose.Schema({
+   urlCode:{
+       type:String,
+     
+    
+   },
+   longUrl:{
+       type:String,
+       required:true,
+       trim:true,
+   },
+   shortUrl:{
+       type:String,
+       trim:true,
+       unique:true
+
+   }
+
+
+   
+     
+    
+},{timestamps:true})
+
+
+module.exports = mongoose.model("Url",urlSchema)
